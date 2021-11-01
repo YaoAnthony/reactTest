479aaf2790585c7a945102481782fe16aacc0750
Aokfine <zhifengyao731@gmail.com>
2021-10-31 20:21:19 -0400

    test3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import logo from './logo.svg';
-import './App.css';
+import './common/generalSetting.css';
+//import React, { Fragment } from 'react';
+//import { HashRouter, Route, Switch, Redirect } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      Here is test 2.
+      Here is test 3.
     </div>
   );
+  // return (
+  //   <Fragment>
+  //     <HashRouter>
+  //       <Switch>
+  //         <Route path="/login" component={Login} />
+  //         <Route path="/home" component={Home} />
+  //         <Route exact path="/" component={Home} />
+  //         <Redirect to={"/home"} />
+  //       </Switch>
+  //     </HashRouter>
+  //   </Fragment>
+  // )
 }
 
 export default App;
